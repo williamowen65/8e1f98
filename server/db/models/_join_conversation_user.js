fed340e402a99ddbc8fd4a03c4aae797ce3f4cfc
William Owen <islandhopper152@gmail.com>
2022-05-06 03:39:07 +0000

    Feat (DRAFT)/database redesign
    - created a join table for conversations and users
    - updated associations on `models/index`
    - updated semantic method names on conversation
        (b/c `findConversations` was now a tasks for the join table)
    - updated conversation model methods
    - drafted join table model methods

---
+++ server/db/models/_join_conversation_user.js
@@ -0,0 +1,7 @@
+const { Op, Sequelize } = require("sequelize");
+const db = require("../db");
+
+const _Join_Conversation_User = db.define("_join_conversation_user", {});
+
+
+module.exports = _Join_Conversation_User;
